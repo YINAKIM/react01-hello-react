402f4650b3e764910a4057411085fc5867883c12
YINAKIM <yinaofficeg@gmail.com>
2022-01-20 12:57:18 +0900

    함수형 컴포넌트에 props사용하기 1

---
+++ src/MyComponent.js
@@ -0,0 +1,18 @@
+const MyComponent = ({ name, children }) => {
+    //const { name, children } = props; //  비구조화할당으로 name이라는 상수에 name값이 있고, children이라는 상수에 children값이 있다.
+    return (
+        <div>
+            Hi, 내 이름은 {name}다. <br />
+            children값은 {children}<br />
+            이다.
+            <hr />
+            표현식안에 props. 을안쓰고 prop명으로 쓸 수 있다.
+        </div>
+    );
+};
+
+MyComponent.defaultProps={
+    name:"이나킴"
+
+};
+export default MyComponent;
