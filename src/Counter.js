6a64350ae347b977d8d2dba3d1954430315e8fba
YINAKIM <yinaofficeg@gmail.com>
2022-01-24 01:25:18 +0900

    state객체 안에 값이 여러개일 때
    * setState()호출 : 인자로 전달된 state객체 안의 값만 변경되는 방식이다.

--- src/Counter.js
+++ src/Counter.js
@@ -2,25 +2,31 @@ import {Component} from "react";
 
 class Counter extends Component {
 
-    // 생성자메서드로 state설정
-    constructor(props){
+    // [방법1] 생성자메서드로 state 초기값 설정
+   /* constructor(props){
         super(props); // 클래스컴포넌트의 생성자메서드에서 반드시 호출(React.Component.md 메모참고)
 
         // state의 초기값 설정 : 반드시 {객체}형식으로
         // 직접 할당할 수 있는 곳은 생성자 뿐, 생성자에서 할당, 생성자 밖에서this.setState로 사용한다.
         this.state = {
-            number:0
+            number:0, fixedNumber:0
         };
     }
+   */
 
+    // [방법1] 생성자메서드로 state 초기값 설정
+    state = {
+        number:0, fixedNumber:0
+    };
 
     // rneder함수안에서 return 엘리먼트
     render() {
-        const {number} = this.state; // 클래스컴포넌트에서 state조회 : 생성자 밖에서 this.state로 조회
+        const {number,fixedNumber} = this.state; // 클래스컴포넌트에서 state조회 : 생성자 밖에서 this.state로 조회
 
         return(
             <div>
-                <h1>{number}</h1>
+                <h1>바뀌는 state값 : {number}</h1>
+                <h1>바뀌지않는 state값 : {fixedNumber}</h1>
                 <button
                 // 화살표함수로 onClick 이벤트 바인딩
                 onClick={
