7d005e8da1af5933362b788db1a6262dd6656b40
YINAKIM <yinaofficeg@gmail.com>
2022-01-26 14:14:55 +0900

    6장 컴포넌트 반복 - key 추가
     (Virtual DOM 효율적으로 리렌더링 할 수 있도록 하기)
    
     key가 없이도 렌더링은 된다. 그러나 있으면 더 빠르게 렌더링할 수 있다.
     ( 브라우저 콘솔에서 Warning: Each child in a list should have a unique "key" prop. )
    
     - key가 없으면 ?? VDOM을 비교하는 과정에서 리스트를 순차적으로 비교, 변화감지 (약간 DB에서 풀스캔..양이 많을수록 인덱스스캔보다 느리다)
    
     - key가 있으면 ?? 이 key값을 사용하여 어떤변화가 일어났는지 더욱 빠르게 감지  (약간 DB에서 인덱스캔..양이 많을수록 풀스캔보다 빠르다)

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -5,8 +5,13 @@ const IterationSample = () => {
     // map(
     //  새로운배열요소 생성하는콜백함수(현재처리중인요소, 현재요소의index, 현재요소의원본array), thisArg(필요시this바인딩)
     // );
-    const nameList = names.map(name => <li>{name}</li>);
+    const nameList = names.map(  (name,index) => <li key={index}>{name}</li> );
 
+    /* key가 없이도 렌더링은 된다. 그러나 있으면 더 빠르게 렌더링할 수 있다.
+       ( 브라우저 콘솔에서 Warning: Each child in a list should have a unique "key" prop. )
+       - key가 없으면 ?? VDOM을 비교하는 과정에서 리스트를 순차적으로 비교, 변화감지 (약간 DB에서 풀스캔..양이 많을수록 인덱스스캔보다 느리다)
+       - key가 있으면 ?? 이 key값을 사용하여 어떤변화가 일어났는지 더욱 빠르게 감지  (약간 DB에서 인덱스캔..양이 많을수록 풀스캔보다 빠르다)
+    */
     return <ul>{nameList}</ul>;
 };
 
